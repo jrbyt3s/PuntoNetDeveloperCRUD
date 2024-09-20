8010d1a2464810818717262f16239e499fdee76c
jrbyt3s <josee.ramirezi@gmail.com>
2024-09-20 10:01:30 -0500

    add migration with command: add-migration first-add-migration

---
+++ GestionDeCursos/GestionDeCursos.Data/Migrations/20240920145528_First-add-migration.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionDeCursos.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Firstaddmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Management");
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                schema: "Management",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Instructors",
+                schema: "Management",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InstructorName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Quealification = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Experience = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instructors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                schema: "Management",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false),
+                    InstructorId = table.Column<int>(type: "int", nullable: false),
+                    CourseFee = table.Column<int>(type: "int", nullable: false),
+                    CourseDuration = table.Column<int>(type: "int", nullable: false),
+                    CourseStartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BachTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Students_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalSchema: "Management",
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Students_Instructors_InstructorId",
+                        column: x => x.InstructorId,
+                        principalSchema: "Management",
+                        principalTable: "Instructors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_CourseId",
+                schema: "Management",
+                table: "Students",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_InstructorId",
+                schema: "Management",
+                table: "Students",
+                column: "InstructorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students",
+                schema: "Management");
+
+            migrationBuilder.DropTable(
+                name: "Courses",
+                schema: "Management");
+
+            migrationBuilder.DropTable(
+                name: "Instructors",
+                schema: "Management");
+        }
+    }
+}
