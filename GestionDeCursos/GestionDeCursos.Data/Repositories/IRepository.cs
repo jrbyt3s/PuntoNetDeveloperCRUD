fdfaaffb0522d42c7497a443a511d1257c7d1393
jrbyt3s <josee.ramirezi@gmail.com>
2024-09-20 12:02:05 -0500

    Patron repositorio: Se crea y implementa la Interface IRepository.cs luego se crea la clase Repository.cs que Hereda de la interface IRepository.cs

---
+++ GestionDeCursos/GestionDeCursos.Data/Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeCursos.Data.Repositories
+{
+    //Generico: Tipo <TEntity>
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+
+        Task<TEntity> Get( int? id );
+
+        Task Add(TEntity entity);
+
+        void Remove(TEntity entity);
+    }
+}
---
+++ GestionDeCursos/GestionDeCursos.Data/Repositories/Repository.cs
@@ -0,0 +1,39 @@
+﻿using GestionDeCursos.Data.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeCursos.Data.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly ApplicationDbContext Context; // This is a Property of the class
+
+        public Repository(ApplicationDbContext contextParam)
+        {
+            Context = contextParam;
+        }
+
+        public Task Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> Get(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
